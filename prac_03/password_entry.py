f605535b909142b1c9332765534811bcf3cea7e7
Alex <alexander.campbell1@my.jcu.edu.au>
2019-03-16 14:40:00 +1000

    Refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,7 +1,23 @@
 """Alexander Campbell"""
-min_length = 5
-password = input("Please enter a password with a minimum of 5 Characters- ")
-while len(password) < min_length:
+
+
+def main():
+    min_length = 5
+    password = get_password(min_length)
+    stars = star_func(password)
+    print(stars)
+
+
+def star_func(password):
+    stars = "*" * len(password)
+    return stars
+
+
+def get_password(min_length):
     password = input("Please enter a password with a minimum of 5 Characters- ")
-stars = "*" * len(password)
-print(stars)
+    while len(password) < min_length:
+        password = input("Please enter a password with a minimum of 5 Characters- ")
+    return password
+
+
+main()
