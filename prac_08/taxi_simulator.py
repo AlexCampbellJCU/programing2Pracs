e6327ce86d753834fd3a9fbb6e27992b813a27c3
Alex <alexander.campbell1@my.jcu.edu.au>
2019-05-05 22:17:36 +1000

    fix taxi simulator

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -5,7 +5,6 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 def main():
     total_bill = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-
     print("Let's drive!")
     print("q)uit, c)hoose taxi, d)rive")
     main_menu = input(">>> ").lower()
